483ec8f4f276752f531b02514ed98a293b8df5f6
mgahn0706 <mgahn0706@snu.ac.kr>
2026-06-02 20:17:05 +0900

    feat: #7 lives system
    Confirmed: Added a four-life system with large animated fish icons at the bottom of the game screen.
    
    Unclear at first: Confirmed the existing life image assets should be used for active and inactive lives.

--- games/preloadAssets.ts
+++ games/preloadAssets.ts
@@ -2,5 +2,6 @@ export const ALL_GAME_PRELOAD_ASSETS = [
   "/images/main-elevator-1.png",
   "/images/main-elevator-2.png",
   "/images/life-active.png",
+  "/images/life-deactive.png",
   "/sounds/095. Museum - Intermissions.mp3",
 ];
